5f8c7f07ba5ba19ff66de397022679f2d1d7eab2
saurabhdhumal1234 <122085524+saurabhdhumal1234@users.noreply.github.com>
2024-06-14 15:07:22 +0530

    Update backend.tf
    d1

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "terraform.tfstate"
+    bucket         = "newcloud1"
+    key            = "abhi/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
