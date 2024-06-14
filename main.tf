fabbb4f1259bb900a3b3a5ac535733386366a73d
EC2 Default User <ec2-user@ip-172-31-49-150.ec2.internal>
2024-06-14 09:15:37 +0000

    Committing changes to main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  # Pass module variables here
+  ami_value = "ami-0eaf7c3456e7b5b68" 
+  instance_type_value = "t2.micro"
 }
 
 # Other resources and configurations
